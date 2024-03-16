e874a9bca610a8f10c0a140b5999432b21d58b6f
heroyooki <heroyooki@gmail.com>
2024-03-16 21:27:09 +0200

    Improved deployment process

--- backend/api/web/charge_points/handlers/v16.py
+++ backend/api/web/charge_points/handlers/v16.py
@@ -1,7 +1,7 @@
 from ocpp.v16 import ChargePoint as ChargePoint16
 
 from api.web.charge_points.handlers.base import OCPPHandler
-from api.web.charge_points.scenarios.v16.boot_notification import BootNotificationScenario
+from api.web.charge_points.ocpp.scenarios.v16.boot_notification import BootNotificationScenario
 
 
 class OCPP16Handler(
---
+++ run.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+set -a
+source .env
+
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 -s [api|worker]"
+    exit 1
+fi
+
+api_flag=0
+worker_flag=0
+
+while getopts ":s:" opt; do
+    case ${opt} in
+        s )
+            case $OPTARG in
+                api )
+                    api_flag=1
+                    ;;
+                worker )
+                    worker_flag=1
+                    ;;
+                * )
+                    echo "Invalid argument: $OPTARG"
+                    exit 1
+                    ;;
+            esac
+            ;;
+        \? )
+            echo "Invalid option: $OPTARG"
+            exit 1
+            ;;
+        : )
+            echo "Option -$OPTARG requires an argument."
+            exit 1
+            ;;
+    esac
+done
+
+docker system prune -f
+docker volume prune -f
+
+if [ $api_flag -eq 1 ]; then
+    docker build -t everythingcharge-worker .
+    docker-compose up --build -d
+elif [ $worker_flag -eq 1 ]; then
+    docker run -it -d --env-file .env \
+      --network everythingcharge_app-network \
+      --name everythingcharge-worker \
+      -p "$WS_SERVER_PORT:$WS_SERVER_PORT" \
+      everythingcharge-worker
+    echo "Connecting worker to the queue on the '$RABBITMQ_HOST' host ..."
+fi
