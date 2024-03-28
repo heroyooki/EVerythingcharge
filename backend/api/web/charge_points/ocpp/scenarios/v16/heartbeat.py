4ba8e9b352e3e2ce4adf6cdb9ec127337433dd34
heroyooki <heroyooki@gmail.com>
2024-03-28 09:39:38 +0200

    Update the charge_point once a Heartbeat occured

--- backend/api/web/charge_points/ocpp/scenarios/v16/heartbeat.py
+++ backend/api/web/charge_points/ocpp/scenarios/v16/heartbeat.py
@@ -1,10 +1,14 @@
+from typing import Any
+
 from loguru import logger
 from ocpp.routing import on
 from ocpp.v16 import call_result
-from ocpp.v16.enums import Action
+from ocpp.v16.enums import Action, ChargePointStatus
 from propan import Depends
 from propan import apply_types
 
+from api.web.charge_points import get_charge_point_service
+from api.web.charge_points.views import UpdateChargePointPayloadView
 from core.utils import get_formatted_utc
 
 
@@ -14,10 +18,18 @@ class HeartbeatScenario:
     @on(Action.Heartbeat)
     async def on_heartbeat(
             self_,
-            utc_datetime: str = Depends(get_formatted_utc)
+            utc_datetime: str = Depends(get_formatted_utc),
+            service: Any = Depends(get_charge_point_service)
     ):
         logger.info(
             f"Accepted '{Action.Heartbeat}' "
             f"(charge_point_id={self_.id})"
+        )
+        payload = UpdateChargePointPayloadView(
+            status=ChargePointStatus.available
+        )
+        await service.update_charge_point(
+            charge_point_id=self_.id,
+            payload=payload.dict(exclude_unset=True)
         )
         return call_result.HeartbeatPayload(current_time=utc_datetime)
